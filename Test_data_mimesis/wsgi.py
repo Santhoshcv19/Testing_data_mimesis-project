d4b5c16231dac54185b8a3d8cfb316cb688951c7
Santhosh <santhoshcv2004@gmail.com>
2023-11-12 22:43:08 +0530

    Vercel Deployment

--- Test_data_mimesis/wsgi.py
+++ Test_data_mimesis/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test_data_mimesis.settings')
 
 application = get_wsgi_application()
+
+app = application
